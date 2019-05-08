e85d3d0d2a6f36f295584e0cacfe90623228124d
chendingyan <dingyan.chen96@gmail.com>
2019-05-08 13:32:35 +0100

    i2v

---
+++ tag_estimation/tag_estimation.py
@@ -0,0 +1,12 @@
+import i2v
+
+
+
+tag = ['blonde hair','brown hair','black hair','blue hair','pink hair',
+               'purple hair','green hair','red hair','silver hair','white hair','orange hair',
+               'aqua hair','gray hair','long hair','short hair','twintails','drill hair','ponytail','blush',
+               'smile','open mouth','hat','ribbon','glasses','blue eyes','red eyes','brown eyes',
+               'green eyes','purple eyes','yellow eyes','pink eyes','aqua eyes','black eyes','orange eyes',]
+tag_map = dict()
+for i, j in enumerate(tag):
+  tag_map[j] = i
